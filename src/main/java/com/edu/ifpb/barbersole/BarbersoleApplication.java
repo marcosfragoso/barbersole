ed22dca260eb621ef72d529ee4fb186ca6ed4979
Marcos Vinícius Fragoso de Oliveira <marcos.vinicius.fragoso.oliveira@ccc.ufcg.edu.br>
2024-08-10 10:51:23 -0300

    initial commit

---
+++ src/main/java/com/edu/ifpb/barbersole/BarbersoleApplication.java
@@ -0,0 +1,13 @@
+package com.edu.ifpb.barbersole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbersoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbersoleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/ifpb/barbersole/BarbersoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.ifpb.barbersole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbersoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
