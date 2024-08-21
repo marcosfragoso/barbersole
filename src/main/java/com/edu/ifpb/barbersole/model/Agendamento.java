efe29437c35cfc8f34f4b112d66187ce4ad51faf
Marcos Vinícius <marcos.vinicius.fragoso.oliveira@ccc.ufcg.edu.br>
2024-08-21 19:18:49 -0300

    começa agendamentos

--- src/main/java/com/edu/ifpb/barbersole/model/Agendamento.java
+++ src/main/java/com/edu/ifpb/barbersole/model/Agendamento.java
@@ -1,6 +1,7 @@
 package com.edu.ifpb.barbersole.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -39,5 +40,6 @@ public class Agendamento {
 
     @Column(name = "hora")
     @NotBlank
+    @FutureOrPresent
     private String hora;
 }
