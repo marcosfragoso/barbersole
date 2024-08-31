4ef85ad5cce5f7433624e366ea05a7d8ecc230f5
Marcos Vinícius Fragoso de Oliveira <marcos.vinicius.fragoso.oliveira@ccc.ufcg.edu.br>
2024-08-31 00:42:30 -0300

    add script playwright

--- src/main/resources/playwright/cortes.py
+++ src/main/resources/playwright/cortes.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright
 import time
 
 def ler_cortes():
-    abrindo_csv = open('D:\\Users\\Marcos\\Documents\\barbersole\\src\\main\\resources\playwright\\corte.csv', 'r')
+    abrindo_csv = open('D:\\Users\\Marcos\\Documents\\barbersole\\src\\main\\resources\\playwright\\cortes.csv', 'r')
     cortes = csv.reader(abrindo_csv, delimiter=';')
     cortes_lista = []
     
@@ -23,16 +23,15 @@ with sync_playwright() as p:
     navegador = p.chromium.launch(headless=False)
     pagina = navegador.new_page()
     pagina.goto("http://localhost:8080/login")
-
-    # Implementar
-    pagina.fill('//*[@id="inputEmail"]', 'admin@suap.edu.br')
-    pagina.fill('//*[@id="inputPassword"]', 'admin')
-    pagina.keyboard.press('Enter')
-    pagina.locator('//html/body/div/aside/nav/ul[2]/li[2]/a/span').click()
+    pagina.fill('//*[@id="username"]', 'fragoso.ufcg@gmail.com')
+    pagina.fill('//*[@id="password"]', '123456')
+    pagina.locator('xpath=/html/body/section/div/div/div/div[2]/div[1]/form/div[3]/button').click()
+    pagina.goto("http://localhost:8080/agendamentos/agendar")
     for corte in cortes:
-        pagina.fill('//*[@id="nome"]', corte[0])
-        pagina.fill('//*[@id="area"]', corte[1])
-        pagina.locator('//*[@id="cadastro"]/form/button').click()
-    pagina.locator('//*[@id="navbarsExampleDefault"]/a').click()
-    pagina.screenshot(path='screenshots/cadastro-cortes.png', full_page=True)
+        pagina.locator('//*[@id="servico"]').select_option(label=f'{corte[0]}')
+        pagina.locator('//*[@id="barbeiro"]').select_option(label=f'{corte[1]}')
+        pagina.fill('//*[@id="data"]', corte[2])
+        pagina.locator('//*[@id="hora"]').select_option(label=f'{corte[3]}')
+        pagina.locator('xpath=/html/body/div/section/div/div/form/div[5]/button').click()
+    pagina.goto("http://localhost:8080/agendamentos/")
     time.sleep(3)
